c9822539a002198f5056684a906ffa3894c0cebd
Munashe Madziwanyika <142252418+g23m8754@users.noreply.github.com>
2023-10-08 14:28:47 +0200

    Delete PingPongGame.java

--- PingPongGame.java
+++
@@ -1,9 +0,0 @@
-public class PingPongGame {
-    
-    public static void main(String[] args) {
-        // Run the game
-        PingPongFrame game = new PingPongFrame();
-        game.run();
-    }
-
-}
